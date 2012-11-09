f237cc57a1045a8020613a907c7a3cc29eb248d2
Thomas Poulet <dark.flames0@gmail.com>
2012-11-09 07:54:46 +0100

    Move all images in a file
    *change link in texture

--- textures.cpp
+++ textures.cpp
@@ -5,7 +5,7 @@ sf::Texture& Texture(std::string a_fileName)
     static std::map<std::string, sf::Texture> textures;
 
     if(textures.count(a_fileName) == 0)
-		std::cout << "Loaded : " << a_fileName << " : " << textures[a_fileName].loadFromFile("" + a_fileName) << std::endl;
+		std::cout << "Loaded : " << a_fileName << " : " << textures[a_fileName].loadFromFile("images/" + a_fileName) << std::endl;
 
     return textures[a_fileName];
 }
